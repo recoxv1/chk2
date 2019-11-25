cd15d4102d5cd406ca314c0cb6664eda685e126d
Muhammad Suleman Malik <m.malik2495@student.leedsbeckett.ac.uk>
2019-11-25 21:29:16 +0000

    no auth

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Filesystem\Filesystem;
 
-if (App::environment('production')) {
-    URL::forceScheme('https');
-}
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -15,6 +11,5 @@ Route::resource('product', 'Postcontroller');
 Route::patch('specs/{specs}', 'ProjectSpecsController@update');
 Route::post('product/{project}/specs', 'ProjectSpecsController@store');
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
