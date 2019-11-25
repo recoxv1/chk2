91008c95b9d97189a393fe23a612500a552907bd
Muhammad Suleman Malik <m.malik2495@student.leedsbeckett.ac.uk>
2019-11-25 07:35:16 +0000

    Until Auth2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Filesystem\Filesystem;
 
+if (App::environment('production')) {
+    URL::forceScheme('https');
+}
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,10 +14,6 @@ Route::resource('product', 'Postcontroller');
 
 Route::patch('specs/{specs}', 'ProjectSpecsController@update');
 Route::post('product/{project}/specs', 'ProjectSpecsController@store');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
